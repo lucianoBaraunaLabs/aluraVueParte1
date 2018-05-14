e27285c17f3c3adaca8dcfccf4e829875d8fd772
Luciano Baraúna <baraunaluciano@gmail.com>
2018-05-13 21:51:05 -0300

    finalizada cap04_aula_01

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-corpo">
+             <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          </div>
+      </div>
+
       </li>
     </ul>
+
   </div>
 </template>
 
@@ -40,5 +48,48 @@ export default {
 </script>
 
 <style lang="scss">
+.centralizado {
+    text-align: center;
+  }
 
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%
+  }
+
+  /* estilo do painel */
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
