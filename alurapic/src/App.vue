fe50d69ae12944a84ca2ab6b75804ba3e189a8d0
Luciano Baraúna <baraunaluciano@gmail.com>
2018-06-06 07:38:08 -0300

    finalizado cap_06

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="corpo">
+    <meu-menu :rotas="routes"/>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 
 <script>
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 export default {
-
+  components: {
+    'meu-menu' : Menu
+  },
+  
+  data() {
+    return {
+      routes
+    }
+  }
 }
 </script>
 
@@ -15,5 +29,11 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
   }
 </style>
--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes,
+  mode: 'history' // o vue aqui lima o #. No backend ainda é preciso que seja
+  // tratada a url para tirar esse carinha.
+
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
-
-
-
